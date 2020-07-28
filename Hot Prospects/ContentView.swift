8dc36c02cbc15186bd8acea368349d59d1c35595
Nikhil Goel <goepnik@gmail.com>
2020-07-28 17:11:06 -0400

    Initial Commit

---
+++ Hot Prospects/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hot Prospects
+//
+//  Created by Nikhil Goel on 7/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Nikhil Goel on 7/28/20.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
